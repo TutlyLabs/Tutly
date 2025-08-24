59494580f8e8331a5f1b415c1e3f944db67560f1
Uday <udaysagar.mail@gmail.com>
2025-08-24 19:18:05 +0530

    fix: final try

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
   /** We already do linting and typechecking as separate tasks in CI */
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
