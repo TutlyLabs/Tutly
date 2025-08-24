99403166f5dd55422d10182dd4daea2d68a5a482
Uday <udaysagar.mail@gmail.com>
2025-08-24 19:11:15 +0530

    fix: deploy

---
+++ apps/web/next.config.ts
@@ -0,0 +1,27 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /** We already do linting and typechecking as separate tasks in CI */
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
+
+  images: {
+    unoptimized: true,
+    domains: ["*"],
+  },
+
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        path: false,
+        os: false,
+      };
+    }
+
+    return config;
+  },
+};
+
+export default nextConfig;
