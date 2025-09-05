7a1b2deff19abc67b2cb85bd71815a5206a22808
Uday <udaysagar.mail@gmail.com>
2025-09-06 00:15:10 +0530

    chore: fix hot reloading

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -2,26 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+
   /** We already do linting and typechecking as separate tasks in CI */
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
 
+  // transpilePackages: [],
+
   images: {
     unoptimized: true,
     domains: ["*"],
-  },
-
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        fs: false,
-        path: false,
-        os: false,
-      };
-    }
-
-    return config;
   },
 };
 
