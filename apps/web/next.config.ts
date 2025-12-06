55d59ced1174093e854384662ee224f96443ae84
Uday Sagar <udaysagar.mail@gmail.com>
2025-12-06 21:20:50 +0530

    chore: fix output file tracinf

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  outputFileTracingRoot: path.join(__dirname, "../.."),
 
   /** We already do linting and typechecking as separate tasks in CI */
   typescript: { ignoreBuildErrors: true },
