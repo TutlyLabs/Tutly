7162962a82d3dc0015cd32704b05c3ffb46762d6
Uday Sagar <udaysagar.mail@gmail.com>
2025-12-06 20:39:01 +0530

    chore: fix turbo build

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,10 +1,15 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   output: "standalone",
 
   /** We already do linting and typechecking as separate tasks in CI */
   typescript: { ignoreBuildErrors: true },
+
+  turbopack: {
+    root: path.join(__dirname, "../.."),
+  },
 
   // transpilePackages: [],
 
